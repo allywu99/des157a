ab5615ad52d125604484060a49b2bca5e344d8ee
Allyson Wu <awu@ucdavis.edu>
2023-02-14 11:58:14 -0800

    update mad lib 2/14

--- studio1/script.js
+++ studio1/script.js
@@ -2,4 +2,15 @@
     "use strict";
     console.log("reading js");
 
+    let play = document.getElementById("playBtn");
+    let pause = document.getElementById("pauseBtn");
+    let audio = new Audio("./walkingTheme.mp3");
+
+    play.addEventListener("click", ()=>{
+        audio.play();
+    })
+    pause.addEventListener("click", ()=>{
+        audio.pause();
+    })
+
 } )();
