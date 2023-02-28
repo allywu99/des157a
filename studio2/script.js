ce3180336df1b12b5ab4b4304ecce087f2f7b433
Allyson Wu <awu@ucdavis.edu>
2023-02-28 12:53:23 -0800

    added every picture

--- studio2/script.js
+++ studio2/script.js
@@ -2,56 +2,63 @@
     'use strict';
     console.log('reaing js');
 
+    //JS for image slider 
     window.addEventListener('load', function () {
     
-        let sliderContent;
-        let sliderWidth;
-        let sliderContent2;
-        let sliderWidth2;
-        let fullSlider = document.querySelector('.slider1');
-        let fullSlider2 = document.querySelector('.slider2');
-        const clonedSlider = fullSlider.innerHTML;
-        const clonedSlider2 = fullSlider.innerHTML;
-    
-        function animateSlider() {
-            sliderContent = document.querySelector('.a');
-            sliderWidth = sliderContent.offsetWidth;
+        function animateSlider(element, direction){
+            const sliderContent = document.querySelector(`${element} .a`);
+            const sliderWidth = sliderContent.offsetWidth;
             const cloned = sliderContent.cloneNode(true);
             cloned.className = "b";
-            document.querySelector('#slider').appendChild(cloned);
-            document.querySelector('#slider').style.left = `-${sliderWidth}px`;
-            repeatAnimation();
+            document.querySelector(`${element} .slider`).appendChild(cloned);
+            let root = document.querySelector(':root');
+            const endLeftPos = `-${sliderWidth}px`;    
+            root.style.setProperty('--sliderwidth', endLeftPos);
+            document.querySelector(`${element} .slider`).classList.add(`${direction}`);
         }
     
-        animateSlider();
-        function repeatAnimation() {
-            fullSlider.addEventListener("transitionend", function () {
-                console.log('going around again!');
-                document.querySelector('.slider1').innerHTML = clonedSlider;
-                fullSlider = document.querySelector('#slider');
-                animateSlider();
-            });
-        }
-
-        function animateSlider2() {
-            sliderContent2 = document.querySelector('.c');
-            sliderWidth2 = sliderContent2.offsetWidth;
-            const cloned2 = sliderContent2.cloneNode(true);
-            cloned2.className = "d";
-            document.querySelector('#slider2').appendChild(cloned2);
-            document.querySelector('#slider2').style.left = `-${sliderWidth2}px`;
-            repeatAnimation2();
-        }
-    
-        animateSlider2();
-        function repeatAnimation2() {
-            fullSlider2.addEventListener("transitionend", function () {
-                console.log('going around again!');
-                document.querySelector('.slider2').innerHTML = clonedSlider2;
-                fullSlider = document.querySelector('#slider2');
-                animateSlider2();
-            });
-        } 
+        animateSlider('#one', 'left');
+        animateSlider('#two', 'right');
         
     });
+
+    //JS for scroll animation
+    ScrollReveal({
+        reset: true,
+        distance: '60px',
+        duration: 2500,
+        delay: 400
+    });
+
+    ScrollReveal().reveal(".title1", {delay:400, origin: 'left'});
+    ScrollReveal().reveal(".chunk1 .image", {delay:500, origin: 'bottom', interval: 50});
+    ScrollReveal().reveal(".chunk1 .text-box", {delay:550, origin: 'right'});
+
+    //creating the overlays for the images 
+    const openBtns = document.querySelectorAll('.open');
+    const closeBtns = document.querySelectorAll('.close');
+    for (const eachBtn of openBtns){
+        eachBtn.addEventListener('click', function(event){
+            event.preventDefault();
+            const thisBtn = event.target.id;
+            document.getElementById(`ol-${thisBtn}`).className = 'overlay showing';
+        });
+    }
+
+    for (const eachBtn of closeBtns){
+        eachBtn.addEventListener('click', function(event){
+            event.preventDefault();
+            document.querySelector('.showing').className = 'overlay hidden';
+        });
+    }
+
+    //JS before/after slider
+    document.querySelector(".compare #compare-ip").addEventListener("input", function(e){
+        document.querySelector(".compare .before .after").style.width = (+e.target.value) + "px";
+    }) 
+
+    document.querySelector(".compare #compare-ip").addEventListener("change", function(e){
+        document.querySelector(".compare .before .after").style.width = (+e.target.value) + "px";
+    }) 
+
 })();
